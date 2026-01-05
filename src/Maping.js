47cfb2030255031526853e55510f734c9f71c195
Mokhtar Mohammed <mokhtarmohammed101@gmail.com>
2026-01-05 06:31:24 +0200

    edie maping

--- src/Maping.js
+++ src/Maping.js
@@ -11,3 +11,22 @@
 //     role VARCHAR(20),
 //     password VARCHAR(255)
 // );
+
+
+// CREATE TABLE IF NOT EXISTS UserPhones (
+//     id INT PRIMARY KEY AUTO_INCREMENT,
+//     phone VARCHAR(20),
+//     user_id INT,
+//     FOREIGN KEY (user_id) REFERENCES Users(id)
+// );
+
+
+// CREATE TABLE IF NOT EXISTS Products (
+//     id INT PRIMARY KEY AUTO_INCREMENT,
+//     name VARCHAR(100),
+//     price DECIMAL(10,2),
+//     stock INT,
+//     isDeleted BOOLEAN DEFAULT FALSE,
+//     user_id INT,
+//     FOREIGN KEY (user_id) REFERENCES Users(id)
+// );
